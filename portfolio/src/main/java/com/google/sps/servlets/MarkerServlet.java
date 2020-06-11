5cfa37b71cf25d4851f7d284605fe3418e880fa4
kashisharora <kashisharora@google.com>
2020-06-11 15:38:27 +0000

    Implement doPost() for MarkerServlet

--- portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
@@ -15,24 +15,89 @@
 package com.google.sps.servlets;
 
 import com.google.sps.data.Marker;
+import com.google.sps.utilities.InputCleaner;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import java.util.Optional;
 
 /** Handles fetching and saving markers data. */
 @WebServlet("/markers")
 public class MarkerServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
 
-  }
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        //use UTF-8 encoding to support different languages
+        try {
+            request.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Encoding failed.");
+        }
+        
+        //if the latidude is null, set the default to an invalid latitude
+        double lat = Double.parseDouble(getParameter(request, "lat").orElse("100"));
+        //if the longitude is null, set the default to an invalid longitude
+        double lng = Double.parseDouble(getParameter(request, "lng").orElse("200"));
+        String description = Jsoup.clean(getParameter(request, "description").orElse(""), Whitelist.none());
 
-  }
+        if(lat < -90 || lat > 90) {
+            response.getWriter().println("Invalid value for latitude. Must be inbetween -90 and 90.");
+            response.sendRedirect("/map.html");
+            return;
+        }
+
+        if(lng < -180  || lng > 180) {
+            response.getWriter().println("Invalid value for longitude. Must be inbetween -180 and 180.");
+            response.sendRedirect("/map.html");
+            return;
+        }
+
+        Marker marker = new Marker(lat, lng, description);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Entity markerEntity = createMarkerEntity(marker);
+        datastore.put(markerEntity);
+    }
+
+    /**
+    * Creates a new Marker entity with data from a user placed marker.
+    *  @return Entity
+    */
+    private Entity createMarkerEntity(Marker marker) {
+    
+
+        Entity markerEntity = new Entity("Marker");
+        String description = InputCleaner.clean(marker.getDescription());
+
+        markerEntity.setProperty("lat", marker.getLat());
+        markerEntity.setProperty("lng", marker.getLng());
+        markerEntity.setProperty("description", description);
+
+        return markerEntity;
+
+    }
+
+
+    /**
+    * @return an Optional of the request parameter
+    */
+    private Optional<String> getParameter(HttpServletRequest request, String name) {
+        String value = request.getParameter(name);
+        return Optional.ofNullable(value);
+    }
 
 }
