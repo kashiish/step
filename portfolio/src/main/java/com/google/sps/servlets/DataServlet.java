6a49fb40775392fafee8f31d5b7b087ae9576982
kashisharora <kashisharora@google.com>
2020-06-15 22:24:44 +0000

    Query datastore to get number of Like entities for comment

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -138,12 +138,12 @@ public class DataServlet extends HttpServlet {
         
         Comment comment;
 
+        long id = entity.getKey().getId();
         String name = (String) entity.getProperty("name");
         String message = (String) entity.getProperty("message");
         String email = (String) entity.getProperty("email");
         long timestamp = (long) entity.getProperty("timestamp");
-        long numLikes = (long) entity.getProperty("numLikes");
-        long id = entity.getKey().getId();
+        long numLikes = getNumLikesForComment(datastore, id);
         boolean isLiked = isCommentLikedByUser(datastore, id);
         String languageCode = translator.detectLanguage(message);
         boolean isAuthor = isUserAuthorOfComment(datastore, id);
@@ -165,6 +165,19 @@ public class DataServlet extends HttpServlet {
 
         return comment;
 
+    }
+
+    /**
+    * Gets the count of Like entities with commentId that is equal to the commentId parameter.
+    * @return long
+    */
+    private long getNumLikesForComment(DatastoreService datastore, long commentId) {
+
+        Query query = new Query("Like").setFilter(FilterOperator.EQUAL.of("commentId", commentId));
+
+        PreparedQuery pq = datastore.prepare(query);
+
+        return pq.countEntities();
     }
 
     /**
@@ -255,7 +268,7 @@ public class DataServlet extends HttpServlet {
         commentEntity.setProperty("message", message);
         commentEntity.setProperty("email", email);
         commentEntity.setProperty("timestamp", timestamp);
-        commentEntity.setProperty("numLikes", 0);
+        // commentEntity.setProperty("numLikes", 0);
         commentEntity.setProperty("userId", userId);
 
         return commentEntity;
