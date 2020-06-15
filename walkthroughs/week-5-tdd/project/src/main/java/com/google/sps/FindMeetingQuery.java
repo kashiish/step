b8bce25467e78037ff7b3a078b7ff1a152dc4629
kashisharora <kashisharora@google.com>
2020-06-15 21:27:08 +0000

    Get available times for meeting (not looking at attendees)

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -18,20 +18,73 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public final class FindMeetingQuery {
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-        ArrayList<TimeRange> availableTimes = new ArrayList<TimeRange>();
+        //if the duration of the meeting is over a day --> no available times
+        if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+            return new ArrayList<TimeRange>();
+        }
 
         //if there are no events or no attendees, the entire day is available
         if(events.size() == 0 || request.getAttendees().size() == 0) {
-            availableTimes.add(TimeRange.WHOLE_DAY);
-            return availableTimes;
+            return Arrays.asList(TimeRange.WHOLE_DAY);;
         }
 
-        ArrayList<Event> sortedEventsList = sortEventsByTime(events);
+        return getAvailableTimes(request);
 
+    }
+
+    /**
+    * Gets all the available time ranges for the meeting request. 
+    * @return ArrayList<TimeRange>
+    */
+    private ArrayList<TimeRange> getAvailableTimes(MeetingRequest request) {
+        ArrayList<TimeRange> availableTimes = new ArrayList<TimeRange>();
+
+        ArrayList<Event> sortedEventsList = sortEventsByStartTime(events);
+
+        //start time of the time range
+        int startTime = TimeRange.START_OF_DAY;
+        long duration = request.getDuration();
+        int i = 0;
+
+        while(startTime + duration <= TimeRange.END_OF_DAY) {
+            int endTime = startTime;
+
+            TimeRange currentEventTime = sortedEventsList.get(i).getWhen();
+
+            //currentEventTime.start() > startTime + duration checks if there is enough time for the meeting
+            while(currentEventTime.start() > startTime + duration && i < sortedEventsList.size()) {
+                i++;
+                //set the end time of the time range to the start of this meeting
+                endTime = currentEventTime.start();
+                if(i < sortedEventsList.size()) {
+                    currentEventTime = sortedEventsList.get(i).getWhen();
+                }
+            }
+
+            //if the endTime of the time range (which was set to startTime) is greater than or equal to the duration of the meeting, it can be added as a new time range
+            if(endTime >= startTime + duration) {
+                availableTimes.add(TimeRange.fromStartEnd(startTime, endTime, false));
+            }
+
+            //if we haven't looked at all events, set the next time range's start time to the end of the current event 
+            if(i < sortedEventsList.size()) {
+                startTime = currentEventTime.end();
+            }
+
+            //if we've looked at all meetings and there is enough time (until the end of the day) for the new meeting, create a new time range until the end of the day
+            if(i == sortedEventsList.size() && TimeRange.END_OF_DAY - startTime >= duration) {
+                availableTimes.add(TimeRange.fromStartEnd(currentEventTime.end(), TimeRange.END_OF_DAY, true));
+                break;
+            }
+
+        }
+
+        return availableTimes;
 
     }
 
